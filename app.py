ec0d5505e92c80444390aaca222827e621c24b2c
vi ann <24002195@myrp.edu.sg>
2026-01-27 16:21:33 +0800

    update app.py with subtract, times, divide

--- app.py
+++ app.py
@@ -7,13 +7,27 @@ calc = Calculator()
 @app.route('/', methods=['GET', 'POST'])
 def index():
     result = ""
+
     if request.method == 'POST':
         num1 = float(request.form.get('num1'))
         num2 = float(request.form.get('num2'))
         operation = request.form.get('operation')
-        
+
         if operation == 'add':
             result = calc.add(num1, num2)
+
+        elif operation == 'subtract':
+            result = calc.subtract(num1, num2)
+
+        elif operation == 'multiply':
+            result = calc.multiply(num1, num2)
+
+        elif operation == 'divide':
+            if num2 == 0:
+                result = "Cannot divide by zero"
+            else:
+                result = calc.divide(num1, num2)
+
     return render_template('index.html', result=result)
 
 if __name__ == "__main__":
