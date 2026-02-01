154ad34969e32eb3dd24145db6266f7b79b2b454
vi ann <24002195@myrp.edu.sg>
2026-02-01 17:37:59 +0800

    add auto update script so that render link and localhost link auto syncs

---
+++ scripts/auto-update.ps1
@@ -0,0 +1,20 @@
+param(
+    [string]$RepoPath = "C:\Users\Vi Ann\c270todolist",
+    [string]$ServiceName = "yesdaddy-calc-stack_todolist-app",
+    [string]$ImageName = "c270todolist:latest"
+)
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
+Set-Location $RepoPath
+
+git fetch --quiet
+$localHead = git rev-parse HEAD
+$remoteHead = git rev-parse @{u}
+
+if ($localHead -ne $remoteHead) {
+    git pull --ff-only
+    docker build -t $ImageName .
+    docker service update --force $ServiceName
+}
